280f257f3bc2516a03c1ebc10733d9dfa57fceac
Stanislava <st.minchuk@gmail.com>
2019-10-24 10:50:32 +0300

    Initial Commit

---
+++ EventfulViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EventfulViewer
+//
+//  Created by Станислава on 24/10/2019.
+//  Copyright © 2019 Stminchuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
