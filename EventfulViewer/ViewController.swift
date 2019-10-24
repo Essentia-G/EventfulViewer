58b350f71bc2d08203f68f69e8a55ed1062db180
Stanislava Essentia <st.minchuk@gmail.com>
2019-10-24 15:22:17 +0300

    Icons paths fix

--- EventfulViewer/ViewController.swift
+++ EventfulViewer/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var searchField: UITextField!
+    @IBOutlet weak var mapKitView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+        configButton()
+    }
+    
+    func configButton() {
+//        searchButton.backgroundColor = .clear
+//        searchButton.layer.cornerRadius = 5
+//        searchButton.layer.borderWidth = 0.5
+//        searchButton.layer.borderColor = UIColor.black.cgColor
+        searchButton.imageView?.contentMode = .scaleAspectFit
     }
 
 
