804e844f5e15b37b3c5c5c9eb26e77b18e82dac7
Stanislava Essentia <st.minchuk@gmail.com>
2020-02-03 15:15:23 +0300

    Refactoring

--- EventfulViewer/Networking/EventDetails.swift
+++ EventfulViewer/Networking/EventDetails.swift
@@ -9,18 +9,18 @@
 import Foundation
 
 struct EventDetail: Codable {
-    
-    var id: String
-    var title: String
-    var latitude: String
-    var longitude: String
-    var url: String
-    var venueAddress: String
-    var regionName: String
-    var image: Image?
-    var description: String
-    var startTime: String
-    
+
+    let id: String
+    let title: String
+    let latitude: String
+    let longitude: String
+    let url: String
+    let venueAddress: String
+    let regionName: String
+    let image: Image?
+    let description: String
+    let startTime: String
+
         init(from decoder: Decoder) throws {
             let container = try decoder.container(keyedBy: CodingKeys.self)
             self.id = (try? container.decode(String.self, forKey: .id)) ?? ""
@@ -32,22 +32,6 @@ struct EventDetail: Codable {
             self.description = (try? container.decode(String.self, forKey: .description)) ?? ""
             self.startTime = (try? container.decode(String.self, forKey: .startTime)) ?? ""
             self.url = (try? container.decode(String.self, forKey: .url)) ?? ""
-            self.image = (try? container.decode(Image?.self, forKey: .image)) 
+            self.image = (try? container.decode(Image?.self, forKey: .image))
         }
-    
-    enum CodingKeys: String, CodingKey {
-        case id
-        case title
-        case latitude
-        case longitude
-        case url
-        case venueAddress = "venue_address"
-        case regionName = "region_name"
-        case description
-        case startTime = "start_time"
-        case image
-        
-    }
-    
-    
 }
